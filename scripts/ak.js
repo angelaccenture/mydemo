5ff434fc3ea9504e8209b656576c3641698715a9
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-01-23 10:48:48 -0800

    try this i think

--- scripts/ak.js
+++ scripts/ak.js
@@ -50,7 +50,8 @@ export async function loadBlock(block) {
   const { classList } = block;
   const name = classList[0];
   block.dataset.blockName = name;
-  const blockPath = `/blocks/${name}/${name}`;
+  const blockPath = "";
+  blockPath = `/blocks/${name}/${name}`;
   const loaded = [new Promise((resolve) => {
     (async () => {
       try {
