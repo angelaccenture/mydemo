2e7a6abdc6000be760ef5b40b69ce94fabd3cb13
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-01-23 11:02:12 -0800

    temp work around to see if fixes errors in ue

--- scripts/ak.js
+++ scripts/ak.js
@@ -50,7 +50,6 @@ export async function loadBlock(block) {
   const { classList } = block;
   const name = classList[0];
   block.dataset.blockName = name;
-  const blockPath = "";
   blockPath = `/blocks/${name}/${name}`;
   const loaded = [new Promise((resolve) => {
     (async () => {
