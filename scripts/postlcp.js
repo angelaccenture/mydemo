74b6fee363dc1aef3d231b62fcbc364647904b49
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-01-30 09:20:10 -0800

    revert all back - try something else

--- scripts/postlcp.js
+++ scripts/postlcp.js
@@ -3,6 +3,4 @@ import { loadBlock } from './ak.js';
 export default async function loadPostLCP() {
   const header = document.querySelector('header');
   if (header) await loadBlock(header);
-  const footer = document.querySelector('footer');
-  if (footer) await loadBlock(footer);
 }
