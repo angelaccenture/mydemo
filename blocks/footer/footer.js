0454c780dd36a3c240d4d2172f597b9435cb7c09
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-01-30 09:12:11 -0800

    updates

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,6 +8,7 @@ const FOOTER_PATH = '/fragments/nav/footer';
  * @param {Element} el The footer element
  */
 export default async function init(el) {
+  console.log("generate from footer.js not utils")
   const { locale } = getConfig();
   const footerMeta = getMetadata('footer');
   const path = footerMeta || FOOTER_PATH;
