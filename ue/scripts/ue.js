f43b73893c9cce07dee4ab3e3d4a9c69e507a65a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-03 14:02:49 -0800

    un-comment and bring code back

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -8,7 +8,7 @@
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
-
+ */
 
 import { showSlide } from '../../blocks/carousel/carousel.js';
 import { moveInstrumentation } from './ue-utils.js';
@@ -122,4 +122,3 @@ export default () => {
   setupObservers();
   setupUEEventHandlers();
 };
- */
