d34175ddb3ba739d9fe7d6f893db9e723e6d18e5
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-05 11:59:33 -0800

    syntext

--- scripts/utils/error.js
+++ scripts/utils/error.js
@@ -7,7 +7,7 @@ const { codeBase } = getConfig();
 export default async function error(ex, el) {
   // eslint-disable-next-line no-console
   console.log("error.js" + ue);
-  if (el && ENV !== 'prod' || ue !== true) {
+  if (el && ENV !== 'prod' || ue != true) {
     console.log("Should not be showing");
     await loadStyle(`${codeBase}/styles/error.css`);
     const wrapper = document.createElement('div');
