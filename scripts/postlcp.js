e8d59292f4d7d02808770f80abefabb2d4f1d823
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-05 12:31:35 -0800

    try this

--- scripts/postlcp.js
+++ scripts/postlcp.js
@@ -3,4 +3,6 @@ import { loadBlock } from './ak.js';
 export default async function loadPostLCP() {
   const header = document.querySelector('header');
   if (header) await loadBlock(header);
+    const footer = document.querySelector('footer');
+  if (footer) await loadBlock(footer);
 }
