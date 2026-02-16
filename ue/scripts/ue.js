1b897f56acb913137b2a3f1a8580e333b348a9f3
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-16 10:53:49 -0800

    updates

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -2,6 +2,79 @@ import { moveInstrumentation } from './ue-utils.js';
 
 const setupObservers = () => {
   console.log("setupObservers - not needed yet");
+  const mutatingBlocks = document.querySelectorAll('div.card, div.carousel, div.accordion');
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === 'childList' && mutation.target.tagName === 'DIV') {
+        const addedElements = mutation.addedNodes;
+        const removedElements = mutation.removedNodes;
+
+        // detect the mutation type of the block or picture (for cards)
+        const type = mutation.target.classList.contains('cards-card-image') ? 'cards-image' : mutation.target.attributes['data-aue-model']?.value;
+
+        switch (type) {
+          case 'card':
+            // handle card div > li replacements
+            console.log("card yes");
+
+            /*if (addedElements.length === 1 && addedElements[0].tagName === 'UL') {
+              const ulEl = addedElements[0];
+              const removedDivEl = [...mutation.removedNodes].filter((node) => node.tagName === 'DIV');
+              removedDivEl.forEach((div, index) => {
+                if (index < ulEl.children.length) {
+                  moveInstrumentation(div, ulEl.children[index]);
+                }
+              });*/
+            }
+            break;
+          case 'cards-image':
+            // handle card-image picture replacements
+            if (mutation.target.classList.contains('cards-card-image')) {
+              const addedPictureEl = [...mutation.addedNodes].filter((node) => node.tagName === 'PICTURE');
+              const removedPictureEl = [...mutation.removedNodes].filter((node) => node.tagName === 'PICTURE');
+              if (addedPictureEl.length === 1 && removedPictureEl.length === 1) {
+                const oldImgEL = removedPictureEl[0].querySelector('img');
+                const newImgEl = addedPictureEl[0].querySelector('img');
+                if (oldImgEL && newImgEl) {
+                  moveInstrumentation(oldImgEL, newImgEl);
+                }
+              }
+            }
+            break;
+          case 'accordion':
+            if (addedElements.length === 1 && addedElements[0].tagName === 'DETAILS') {
+              moveInstrumentation(removedElements[0], addedElements[0]);
+              moveInstrumentation(removedElements[0].querySelector('div'), addedElements[0].querySelector('summary'));
+            }
+            break;
+          case 'carousel':
+            if (removedElements.length === 1 && removedElements[0].attributes['data-aue-model']?.value === 'carousel-item') {
+              const resourceAttr = removedElements[0].getAttribute('data-aue-resource');
+              if (resourceAttr) {
+                const itemMatch = resourceAttr.match(/item-(\d+)/);
+                if (itemMatch && itemMatch[1]) {
+                  const slideIndex = parseInt(itemMatch[1], 10);
+                  const slides = mutation.target.querySelectorAll('li.carousel-slide');
+                  const targetSlide = Array.from(slides).find((slide) => parseInt(slide.getAttribute('data-slide-index'), 10) === slideIndex);
+                  if (targetSlide) {
+                    moveInstrumentation(removedElements[0], targetSlide);
+                  }
+                }
+              }
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  });
+
+  mutatingBlocks.forEach((cardsBlock) => {
+    observer.observe(cardsBlock, { childList: true, subtree: true });
+  });
+};
+
 };
 
 const setupUEEventHandlers = () => {
