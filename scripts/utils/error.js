642b667dc554552ca0129ee6712ba43843523f97
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-16 12:16:58 -0800

    uggg its int he or

--- scripts/utils/error.js
+++ scripts/utils/error.js
@@ -6,18 +6,8 @@ const { codeBase } = getConfig();
 export default async function error(ex, el) {
   // eslint-disable-next-line no-console
   const daHostName = window.location.hostname.includes('ue.da.live');
-  if (daHostName !== true) {
-    console.log("this should not show in UE, but should show in normal page")
-  }
-  else {
-    console.log("this should show up in UE")
-  }
-
      if ((el && ENV !== 'prod') || (daHostName !== true)) {
-        if (daHostName !== true) {
-            console.log("wtf")
-        }
-      console.log("why is this showing up in UE???");
+      console.log("newest why is this showing up in UE???");
         await loadStyle(`${codeBase}/styles/error.css`);
         const wrapper = document.createElement('div');
         wrapper.className = 'has-error';
