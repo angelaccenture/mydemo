d780097d1eaeeff655599d4362a7a875638b86e3
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-16 12:27:39 -0800

    updates

--- scripts/utils/error.js
+++ scripts/utils/error.js
@@ -7,7 +7,7 @@ export default async function error(ex, el) {
   // eslint-disable-next-line no-console
   const daHostName = window.location.hostname.includes('ue.da.live');
      if ((el && ENV !== 'prod') || (daHostName !== true)) {
-      console.log("newest why is this showing up in UE???");
+      console.log("newest cache why is this showing up in UE???");
       if (daHostName !== true) {
         await loadStyle(`${codeBase}/styles/error.css`);
         const wrapper = document.createElement('div');
