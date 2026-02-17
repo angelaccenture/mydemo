6609a84bee3e511e7ceb9e02c9973875b31b9991
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-17 13:23:35 -0800

    updates

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,6 +1,20 @@
+const getBtndata = (el) => [...el.childNodes].reduce((rdx, row) => {
+  if (row.children) {
+    const key = row.children[0].textContent.trim().toLowerCase();
+    const content = row.children[1];
+    const text = content.textContent.trim().toLowerCase();
+    if (key && content) rdx[key] = { content, text };
+  }
+  return rdx;
+}, {});
+
 export default function init(el) {
   const btnRoot = el.querySelector(':scope > div');
-  console.log(el);
+    if (!btnRoot) return;
+  const btndata = getBtndata(el);
+  console.log(btndata);
+  //if (btndata.text?.text) handleLayout(metadata.text.text, section, 'text');
+  console.log(btndata.link);
   const buttonTag = document.createElement('button');
   const buttonText = "test";
   buttonTag.textContent = buttonText;
@@ -9,5 +23,10 @@ export default function init(el) {
   
   btnRoot.before(buttonTag);
 
+  const section = el.closest('.section');
+  
 
+ 
+
+  //el.remove();
 }
