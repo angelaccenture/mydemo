5463393b20c6c426fd7a22fdd44409d6db2fdd6c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-17 14:06:20 -0800

    updates

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,5 +1,6 @@
 const getBtndata = (el) => [...el.childNodes].reduce((rdx, row) => {
   if (row.children) {
+    console.log(row.children);
     const key = row.children[0].textContent.trim().toLowerCase();
     const content = row.children[1];
     const text = content.textContent.trim();
