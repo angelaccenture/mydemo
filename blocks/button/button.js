7078b0a03f5e7f7ec66fe4f7f3a5e70e51447b2d
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-17 13:03:31 -0800

    updates

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -3,7 +3,7 @@ export default function init(el) {
   console.log(inner);
   const buttonTag = document.createElement('button');
   const buttonTagAttr = buttonTag.setAttribute("type", "button");
-  inner.insertAdjacentElement('afterbegin', inner);
+  inner.append(buttonTagAttr);
 
   /*Add class name, value and aria-label within the button*/
 
