1a2ae4e4c20aac3b9927ce553d0455e203555df4
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-17 12:47:29 -0800

    update

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,4 +1,8 @@
 export default function init(el) {
   const inner = el.querySelector(':scope > div');
   console.log(inner);
+  const buttonTag = document.createElement('button');
+  const buttonTagAttr = buttonTag.setAttribute("type", "button");
+  /*Add class name, value and aria-label within the button*/
+
 }
