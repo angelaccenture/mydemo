217f2f44b69f841ef27489204ea187f9fd9201b0
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-19 09:27:12 -0800

    updates

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -4,11 +4,13 @@ export default function init(el) {
   const tagMeta = getMetadata('article:tag');
   console.log(tagMeta);
   const tagRoot = el.querySelector(':scope > div');
-
+   // tagAll.forEach(tagelement => {
+   //     btnelement.remove();
+   // });
       const tag = document.createElement('a');
       tag.textContent = tagMeta;
       console.log(tag);
-      //btn.href = btndata.link.text;
-      //btn.setAttribute('aria-label',btndata.label.text);
+      btn.href = 'http://www.getthis.com';
+      btn.setAttribute('aria-label',tagMeta);
       tagRoot.before(tag);
 }
