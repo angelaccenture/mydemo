7701f54cacb1b06e42799c78f66187afd71309d4
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-19 09:43:33 -0800

    trythis

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,5 +1,17 @@
 import { getMetadata } from '../../scripts/ak.js';
 
+function filter(name) {
+  return name
+    .toLowerCase()
+    .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // remove diacritics
+    .replace(/\s/gm, '-') // replace whitespace with -
+    .replace(/&amp;/gm, '') // remove encoded ampersands
+    .replace(/&/gm, '') // remove unencoded ampersands
+    .replace(/\./gm, '') // remove dots
+    .replace(/--+/g, '-'); // remove multiple dashes
+}
+
+
 export default function init(el) {
   console.log(el);
   const tagMeta = getMetadata('article:tag');
@@ -10,8 +22,9 @@ export default function init(el) {
    // });
       const tag = document.createElement('a');
       tag.textContent = tagMeta;
+      const name = tagMeta;
       console.log(tag);
-      tag.href = 'http://www.getthis.com';
+      tag.href = 'https://blogs.microsoft.com/blog/tag/'+${filter(name);
       tag.setAttribute('aria-label',tagMeta);
       tagRoot.before(tag);
 }
