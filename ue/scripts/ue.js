264538163e33578b1d1c5179c6d36d60116ecfc6
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-24 10:26:04 -0800

    updates

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -22,6 +22,7 @@ const setupObservers = () => {
   const mutatingBlocks = document.querySelectorAll('footer, div.cards, div.carousel, div.accordion');
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
+      console.log(mutation.type);
       if (mutation.type === 'childList' && mutation.target.tagName === 'DIV') {
         const addedElements = mutation.addedNodes;
         const removedElements = mutation.removedNodes;
