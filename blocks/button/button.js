8c4c0e52910b9ec2f487d9116363ba76cfebef3a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-25 12:23:00 -0800

    updates to button

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,23 +1,3 @@
 export default function init(el) {
-  //const btnRoot = el.querySelector(':scope > div');
-  //console.log(btnRoot);
-
- /* if (btnRoot) {
-    const btnlink = btnRoot.contains('http://' || 'https://');
-    console.log(btnlink); 
-      console.log(btnRoot.innerHTML);
-      const btn = document.createElement('a');
-      btn.textContent = btndata.text.text;
-     // console.log(btndata.text.parentElement);
-      btn.href = btndata.link.text;
-      btn.setAttribute('aria-label',btndata.text);
-      btnRoot.before(btn);
-  }
-  else {
-    //console.log("Button must have a standard link");
-  } */
- //const btnAll = el.querySelectorAll(':scope > div');
- //btnAll.forEach(btnelement => {
-  //btnelement.remove();
-//});
+    //Code not needed, can use link but this allows us to add multiple styles
 }
