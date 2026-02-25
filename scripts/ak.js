8c2adc07e4f3ed4ce0323ff027c04308b0953db7
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-25 12:57:02 -0800

    updates

--- scripts/ak.js
+++ scripts/ak.js
@@ -76,7 +76,7 @@ function loadTemplate() {
   });
 }
 
-/*function decoratePictures(el) {
+function decoratePictures(el) {
   const pics = el.querySelectorAll('picture');
   for (const pic of pics) {
     const source = pic.querySelector('source');
@@ -88,7 +88,7 @@ function loadTemplate() {
     clone.setAttribute('media', '(min-width: 1440px)');
     pic.prepend(clone);
   }
-}*/
+}
 
 /*
 function decorateButton(link) {
@@ -283,7 +283,7 @@ function decorateDoc() {
 export async function loadArea({ area } = { area: document }) {
   const isDoc = area === document;
   if (isDoc) decorateDoc();
-  //decoratePictures(area);
+  decoratePictures(area);
   const { decorateArea } = getConfig();
   if (decorateArea) decorateArea({ area });
   const sections = decorateSections(area, isDoc);
