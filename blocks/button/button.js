4c5a39fb33d21a4ce288b79a89a060d214074e5b
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-25 12:19:34 -0800

    ops

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -2,8 +2,8 @@ export default function init(el) {
   //const btnRoot = el.querySelector(':scope > div');
   //console.log(btnRoot);
 
-  if (btnRoot) {
-  /*  const btnlink = btnRoot.contains('http://' || 'https://');
+ /* if (btnRoot) {
+    const btnlink = btnRoot.contains('http://' || 'https://');
     console.log(btnlink); 
       console.log(btnRoot.innerHTML);
       const btn = document.createElement('a');
@@ -11,11 +11,11 @@ export default function init(el) {
      // console.log(btndata.text.parentElement);
       btn.href = btndata.link.text;
       btn.setAttribute('aria-label',btndata.text);
-      btnRoot.before(btn);*/
+      btnRoot.before(btn);
   }
   else {
     //console.log("Button must have a standard link");
-  } 
+  } */
  //const btnAll = el.querySelectorAll(':scope > div');
  //btnAll.forEach(btnelement => {
   //btnelement.remove();
