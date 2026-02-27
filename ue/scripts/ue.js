588adc2d158a82ff08cc1cc93be38656cbbf7b6b
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-02-27 10:44:24 -0800

    updates

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -81,9 +81,10 @@ const setupUEEventHandlers = () => {
     if (event.detail.patch.name == 'layoutmode') {
         console.log("layout mode is active");
         console.log(event);
-        const section = event.detail.patch.value;
-        //const section = document.closest('.section');
-        console.log(section);
+        const lm = event.detail.patch.value;
+        if (lm == true) {
+          console.log("turning lm on")
+        }
 
         //Turn it off if the user leaves
     }
