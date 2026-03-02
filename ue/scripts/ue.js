3e3cc239204cbb5e2de26d855bd3e8f58c0c424f
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-02 13:46:09 -0800

    updates

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -78,8 +78,8 @@ const setupUEEventHandlers = () => {
       if (event.detail.patch.value == true) {
         const getsection = event.srcElement.querySelector('div').parentNode;
         console.log(getsection);
-        const parentSection = getsection.style.color = 'red';
-        console.log(parentSection);
+        getsection.style.color = 'red';
+        console.log(getsection.content);
         console.log("need to modify this event");
        console.log(event);
       }
