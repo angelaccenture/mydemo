96ffc8ac0087461ad84b328ec0cbb738b45de6ec
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 11:57:04 -0800

    try this

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,7 +2,8 @@ export default function init(el) {
     console.log("Use advanced Text block");
     const getText = document.querySelectorAll('.default-content');
     getText.forEach(function(element) {
-         console.log(element);
+         console.log(element.childNode);
+
         element.style.color = 'red';
         element.classList.add('active'); // Example of adding another class name
     });
