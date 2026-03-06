c1ef67e6177fa4e82824087dd34150891304693a
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 13:25:46 -0800

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,55 +1,16 @@
 export default function init(el) {
     const getDefault = document.querySelectorAll('.default-content');
 
-const elementsWithBrackets = Array.from(getDefault).filter(element => {
-  // Check if the textContent includes both '[' and ']'
-  const text = element.textContent;
-  return text.includes('[') && text.includes(']');
-});
+    const elementsWithBrackets = Array.from(getDefault).filter(element => {
+    const text = element.textContent;
+    return text.includes('[') && text.includes(']');
+    });
 
 // Log the matching elements
-console.log("what is this?");
-console.log(elementsWithBrackets);
-
-
+    console.log("what is this?");
+    console.log(elementsWithBrackets);
 
     getDefault.forEach(function(element) {    
-        const str = element.childNodes;
-        console.log("original string");  
-        console.log(str);
-        const strings = ["[value1]", "item[value2]", "[value3][]"];
-        console.log("strings");
-        console.log(strings);
-        const extractedValues = [];
-
-            strings.map(str => {
-            const matches = str.match(/\\[(.*?)\\]/g); // Find all matches including brackets
-            if (matches) {
-                matches.forEach(match => {
-                // Remove the brackets to get the value
-                const value = match.replace('[', '').replace(']', '');
-                extractedValues.push(value);
-                });
-            }
-            });
-            console.log("extractedValues");
-        console.log(extractedValues);
-        
-       // defaultChildren.forEach(function(eachEl) {  
-           // const regex = /\[(.*?)\]/;
-           // const findClass = eachEl.match(regex);
-           // if (findClass && findClass.length > 1) {
-            //    console.log(findClass[0]);
-           // }
-
-           /* if (eachEl.textContent.includes('[' && ']')) {
-
-            console.log(eachEl);
-            eachEl.style.color = 'red';
-            }   */
-        // });
-      
-        
-        //element.classList.add('active'); // Example of adding another class name
+       
     });
 }
