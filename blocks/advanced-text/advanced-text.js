05fda6425c531371ae31b2f39c14344259b56f38
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 12:15:52 -0800

    ops

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,7 +2,8 @@ export default function init(el) {
     console.log("Use advanced Text block");
     const getText = document.querySelectorAll('.default-content');
     getText.forEach(function(element) {
-        if (element.includes('[')) {
+        const elementText = element.textContent; 
+        if (elementText.includes('[')) {
             console.log(element);
             element.style.color = 'red';
         }
