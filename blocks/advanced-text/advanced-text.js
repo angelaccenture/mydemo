989208b6ac934c6fe843b577890634babe228740
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 12:38:22 -0800

    try this

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,17 +1,15 @@
 export default function init(el) {
-    console.log("Use advanced Text block");
     const getDefault = document.querySelectorAll('.default-content');
     
     getDefault.forEach(function(element) {    
         const defaultChildren = element.childNodes;
-        console.log(defaultChildren);
          defaultChildren.forEach(function(eachEl) {  
+            if (eachEl.textContent.includes('[' && ']')) {
             console.log(eachEl);
+            eachEl.style.color = 'red';
+            }   
          });
-       if (defaultChildren.textContent.includes('[' && ']')) {
-            console.log(element);
-            element.style.color = 'red';
-        }
+      
         
         //element.classList.add('active'); // Example of adding another class name
     });
