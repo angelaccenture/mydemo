ef51699c72e51c78678e7a5afe2b7e96cdee4b4e
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 11:16:33 -0800

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,5 +1,9 @@
 export default function init(el) {
     console.log("Use advanced Text block");
     const getText = document.querySelectorAll('default-content');
-    console.log(getText);
+    getText.forEach(function(element) {
+         console.log(element);
+        element.style.color = 'red';
+        element.classList.add('active'); // Example of adding another class name
+    });
 }
