c372dfd74008cd57720b2b258b3583e86db1e607
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 12:24:18 -0800

    try this

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,12 +1,12 @@
 export default function init(el) {
     console.log("Use advanced Text block");
-    const getDefault = document.querySelectorAll('.default-content').childNodes;
-    getDefault.forEach(function(element) {
-        const elementText = element.textContent; 
-        if (elementText.includes('[')) {
+    const getDefault = document.querySelectorAll('.default-content');
+    const getClasses = getDefault.textContent.includes('[');
+    getClasses.forEach(function(element) {
+       // if (elementText.includes('[')) {
             console.log(element);
             element.style.color = 'red';
-        }
+        //}
         
 
    
