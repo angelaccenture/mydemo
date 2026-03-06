2d01db9bc185973290675a8f7ee1005587d5a1fa
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 13:39:07 -0800

    try this

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,6 +1,11 @@
 export default function init(el) {
     const getDefault = document.querySelectorAll('.default-content');
 
+    getDefault.forEach(function(element) {  
+        console.log(element.children);  
+       
+    });
+
     const elementsWithBrackets = Array.from(getDefault).filter(element => {
     const text = element.textContent;
     return text.includes('[') && text.includes(']');
