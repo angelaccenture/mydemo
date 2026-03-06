9fe6f8a00ff57cb6bbdfdeb8842dd49922c801c3
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-06 12:13:16 -0800

    lets see

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,9 +2,13 @@ export default function init(el) {
     console.log("Use advanced Text block");
     const getText = document.querySelectorAll('.default-content');
     getText.forEach(function(element) {
-         console.log(element.includes("["));
+        if (element.includes('[')) {
+            console.log(element);
+            element.style.color = 'red';
+        }
+        
 
-        element.style.color = 'red';
+   
         element.classList.add('active'); // Example of adding another class name
     });
 }
