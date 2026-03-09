1233e9217ff32f092b9ba1cd9df980b6f3a8c295
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 12:45:09 -0700

    see evertyhing and rethink

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -3,15 +3,18 @@ export default function init(el) {
     const regex = /\[(.*?)\]/;
 
     allHP.forEach((element, index) => {
+        console.log("Element: " + element);
         const content = element.textContent;
+        console.log("content: " + content);
         const matches = content.match(regex);
+         console.log("matches: " + matches);
         if (matches) {
             const classname = matches[1];
             element.classList.add(classname);
         }
-             const updatedArray = content.map(item => {
-                return item.replace(matches[0], "");
-             });
+            // const updatedArray = content.map(item => {
+             //   return item.replace(matches[0], "");
+            // });
     });
 
 }
