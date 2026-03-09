20194953b0d7ef6c1f1b161e7e2f82f03f06fc4c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 08:40:05 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,29 +1,14 @@
 export default function init(el) {
-    const getDefault = document.querySelectorAll('.default-content');
+    const allHP = document.querySelectorAll('h1, p');
 
-    getDefault.forEach(function(element) {
-        console.log("children")  
-        const getDefaultC = element.children;
-        console.log(getDefaultC);  
+    allHP.forEach(function(element) { 
+        const elementsWithBrackets = Array.from(getDefaultC).filter(element => {
+        const text = element.textContent;
+        return text.includes('[') && text.includes(']');
+        });
 
-
-            const elementsWithBrackets = Array.from(getDefaultC).filter(element => {
-            const text = element.textContent;
-            return text.includes('[') && text.includes(']');
-            });
-       
-    });
-
-    //const elementsWithBrackets = Array.from(getDefault).filter(element => {
-    //const text = element.textContent;
-    //return text.includes('[') && text.includes(']');
-    //});
-
-// Log the matching elements
-    console.log("Within Array");
-    console.log(elementsWithBrackets);
-
-    getDefault.forEach(function(element) {    
+        console.log("elements")  
+        console.log(elementsWithBrackets); 
        
     });
 }
