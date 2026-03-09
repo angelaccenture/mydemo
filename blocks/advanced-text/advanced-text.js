076540f6f24c84b8756e28e027900b235be9e975
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 10:02:13 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,11 +2,25 @@ export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
 
     allHP.forEach(function(element) { 
-        const regex = /\[(.*?)\]/;
-        const findClass = element.match(regex);
-        if (findClass && findClass.length > 1) {
-             console.log(findClass);
-           }
+        
+        const strings = ["[value1]", "item[value2]", "[value3][]"];
+        console.log("strings");
+        console.log(strings);
+        const extractedValues = [];
+     
+     
+        strings.map(str => {
+            const matches = str.match(/\\[(.*?)\\]/g); // Find all matches including brackets
+            if (matches) {
+                matches.forEach(match => {
+                // Remove the brackets to get the value
+                const value = match.replace('[', '').replace(']', '');
+                extractedValues.push(value);
+                });
+            }
+            });
+            console.log("extractedValues");
+            console.log(extractedValues);
         /*const elementsWithBrackets = Array.from(allHP).filter(element => {
             const text = element.textContent;
             return text.includes('[') && text.includes(']');
