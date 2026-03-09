db23a2ae07689d195bc5ac382abd01c701a3e02c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 08:57:12 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,35 +1,18 @@
 export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
 
-    const extractedValues = [];
-
-    allHP.map(str => {
-    const matches = str.match(/\\[(.*?)\\]/g); // Find all matches including brackets
-            if (matches) {
-                matches.forEach(match => {
-                // Remove the brackets to get the value
-                const value = match.replace('[', '').replace(']', '');
-                extractedValues.push(value);
-                });
-            }
-    });
-        console.log("extractedValues");
-        console.log(extractedValues);
-   
-   
-   
-   /* allHP.forEach(function(element) { 
+     allHP.forEach(function(element) { 
         const elementsWithBrackets = Array.from(allHP).filter(element => {
-        const text = element.textContent;
-        return text.includes('[') && text.includes(']');
+            const text = element.textContent;
+            return text.includes('[') && text.includes(']');
         });
 
         console.log("elements")  
         console.log(elementsWithBrackets); 
-
-         console.log("text")  
-        console.log(text); 
-        
       
-    });*/
+    });
+   
+   
+   
+  
 }
