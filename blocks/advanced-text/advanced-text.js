e2f12e92966403ace581801c27e2d914468f1fc4
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 08:54:19 -0700

    uggg

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,7 +1,24 @@
 export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
 
-    allHP.forEach(function(element) { 
+    const extractedValues = [];
+
+    allHP.map(str => {
+    const matches = str.match(/\\[(.*?)\\]/g); // Find all matches including brackets
+            if (matches) {
+                matches.forEach(match => {
+                // Remove the brackets to get the value
+                const value = match.replace('[', '').replace(']', '');
+                extractedValues.push(value);
+                });
+            }
+    });
+        console.log("extractedValues");
+        console.log(extractedValues);
+   
+   
+   
+   /* allHP.forEach(function(element) { 
         const elementsWithBrackets = Array.from(allHP).filter(element => {
         const text = element.textContent;
         return text.includes('[') && text.includes(']');
@@ -14,5 +31,5 @@ export default function init(el) {
         console.log(text); 
         
       
-    });
+    });*/
 }
