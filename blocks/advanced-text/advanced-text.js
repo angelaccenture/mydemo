ba0ca176fdc5c033e8aca6075d153c647596245c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 09:07:51 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -1,18 +1,12 @@
 export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
-    const classNames = [];
 
-    allHP.forEach(element => {
-        const text = element.textContent || element.innerText; 
-        const regex = /\\[(.*?)\\]/g;
-        let match;
-        while ((match = regex.exec(text)) !== null) {
-            classNames.push(match[1]); 
-        }
-    });
+    const elements = document.querySelectorAll('h1, p');
+    const valuesInsideBrackets = [...elements]
+    .map(element => element.textContent.matchAll(/\\[(.*?)\\]/g)) 
+    .flatMap(matches => [...matches].map(match => match[1])); 
 
-    // Display the results
-    console.log(classNames);
+console.log(valuesInsideBrackets); 
    
    
    
