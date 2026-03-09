392ee24bad61ede754e6f82fdfb9af4f3b11ea93
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 11:36:52 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,17 +2,19 @@ export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
     const allExtractedValues = [];
 
-    allHP.forEach(function(element) { 
+    allHP.forEach((element, index) => {
         const content = element.textContent;
         const regex = /\[(.*?)\]/;
         const matches = content.match(regex);
+        console.log(element);
+        console.log(index);
 
         if (matches) {
             console.log("matches");
             console.log(matches);
-            const value = element.substring(1, element.length - 1);
-            allExtractedValues.push(value);
-            console.log("Found value:", value);
+            //const value = element.substring(1, element.length - 1);
+            //allExtractedValues.push(value);
+            //console.log("Found value:", value);
 
             // If matches are found, process them to remove the brackets
             /*matches.forEach(match => {
@@ -25,5 +27,5 @@ export default function init(el) {
         }
     });
 
-console.log("All extracted values:", allExtractedValues);
+//console.log("All extracted values:", allExtractedValues);
 }
