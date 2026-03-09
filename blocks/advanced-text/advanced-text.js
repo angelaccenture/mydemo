486a188dd5f424a3d55c37cb35bce5eef6dfa154
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 08:49:41 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -8,21 +8,8 @@ export default function init(el) {
         });
 
         console.log("elements")  
-        console.log(elementsWithBrackets); 
+        console.log(text); 
 
-        const extractedValues = [];
-         elementsWithBrackets.map(str => {
-            const matches = str.match(/\\[(.*?)\\]/g); // Find all matches including brackets
-            if (matches) {
-                matches.forEach(match => {
-                // Remove the brackets to get the value
-                const value = match.replace('[', '').replace(']', '');
-                extractedValues.push(value);
-                });
-            }
-            });
-            console.log("extractedValues");
-        console.log(extractedValues);
-       
+      
     });
 }
