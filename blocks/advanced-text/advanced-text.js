9d7417956f25e75259546a6e12d327945b034944
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 09:55:06 -0700

    try it this way

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,13 +2,19 @@ export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
 
     allHP.forEach(function(element) { 
-        const elementsWithBrackets = Array.from(allHP).filter(element => {
+        const regex = /\[(.*?)\]/;
+        const findClass = eachEl.match(regex);
+        if (findClass && findClass.length > 1) {
+             console.log(findClass);
+           }
+        /*const elementsWithBrackets = Array.from(allHP).filter(element => {
             const text = element.textContent;
             return text.includes('[') && text.includes(']');
+
         });
         if (elementsWithBrackets) {
             element.classList.add('center');
-        }
+        }*/
     }); 
    
    
