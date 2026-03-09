d6af6fdc4ceb5939d68f9215b3bace7fb4533d00
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 08:46:39 -0700

    pulling the text out

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -9,6 +9,20 @@ export default function init(el) {
 
         console.log("elements")  
         console.log(elementsWithBrackets); 
+
+        const extractedValues = [];
+         elementsWithBrackets.map(str => {
+            const matches = str.match(/\\[(.*?)\\]/g); // Find all matches including brackets
+            if (matches) {
+                matches.forEach(match => {
+                // Remove the brackets to get the value
+                const value = match.replace('[', '').replace(']', '');
+                extractedValues.push(value);
+                });
+            }
+            });
+            console.log("extractedValues");
+        console.log(extractedValues);
        
     });
 }
