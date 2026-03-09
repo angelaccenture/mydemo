a9451770509a0cce6481c23cccec8f61b2ceeb95
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 11:30:50 -0700

    clean this up

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -3,23 +3,25 @@ export default function init(el) {
     const allExtractedValues = [];
 
     allHP.forEach(function(element) { 
-    const content = element.textContent;
-    const regex = /\[(.*?)\]/;
-    const matches = content.match(regex);
+        const content = element.textContent;
+        const regex = /\[(.*?)\]/;
+        const matches = content.match(regex);
 
-    if (matches) {
-        console.log("matches");
-        console.log(matches);
-        // If matches are found, process them to remove the brackets
-        matches.forEach(match => {
-            console.log(match);
-            // Remove the leading '[' and trailing ']'
-            const value = match.substring(1, match.length - 1);
-            allExtractedValues.push(value);
-            console.log("Found value:", value);
-        });
-    }
-});
+        if (matches) {
+            console.log("matches");
+            console.log(matches);
 
-console.log("All extracted values:", allExtractedValues);
+
+            // If matches are found, process them to remove the brackets
+            /*matches.forEach(match => {
+                console.log(match);
+                // Remove the leading '[' and trailing ']'
+                const value = match.substring(1, match.length - 1);
+                allExtractedValues.push(value);
+                console.log("Found value:", value);
+            });*/
+        }
+    });
+
+//console.log("All extracted values:", allExtractedValues);
 }
