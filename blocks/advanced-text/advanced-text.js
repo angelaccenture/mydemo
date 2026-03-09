0c01a8f23c062d3356e2eb5570bf2eed1a3cfa43
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 11:41:50 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -8,19 +8,11 @@ export default function init(el) {
         const matches = content.match(regex);
 
         if (matches) {
-            console.log("matches");
-            console.log(matches);
+            console.log("match value");
             console.log(matches[1]);
-            // If matches are found, process them to remove the brackets
-            /*matches.forEach(match => {
-                console.log(match);
-                // Remove the leading '[' and trailing ']'
-                const value = match.substring(1, match.length - 1);
-                allExtractedValues.push(value);
-                console.log("Found value:", value);
-            });*/
+            console.log("content to move class name to");
+            console.log(element);
         }
     });
 
-console.log("All extracted values:", allExtractedValues);
 }
