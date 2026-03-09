edf693dbc6e67420a1f5ba962c01bc7d9a647f07
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-09 08:41:27 -0700

    updates

--- blocks/advanced-text/advanced-text.js
+++ blocks/advanced-text/advanced-text.js
@@ -2,7 +2,7 @@ export default function init(el) {
     const allHP = document.querySelectorAll('h1, p');
 
     allHP.forEach(function(element) { 
-        const elementsWithBrackets = Array.from(getDefaultC).filter(element => {
+        const elementsWithBrackets = Array.from(allHP).filter(element => {
         const text = element.textContent;
         return text.includes('[') && text.includes(']');
         });
