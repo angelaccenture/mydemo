ee77080cb5d9cf3dac6e4171286491fc09769f5f
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-16 11:03:53 -0700

    try this

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -102,7 +102,7 @@ const setupUEEventHandlers = () => {
   document.addEventListener('aue:content-remove', (removeevent) => {
     console.log("content remove");
     console.log(removeevent);
-    window.editor.api.refresh('body')
+    window.location.reload();
     console.log("refresh");
   });
     document.addEventListener('aue:content-update', (updateevent) => {
