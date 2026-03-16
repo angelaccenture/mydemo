ae78b1a61f0a9570d3b8d47f8e2b67ad9ef53062
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-16 15:39:13 -0700

    updates

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -8,13 +8,12 @@ export default function init(el) {
     if (picPara) {
       picPara.classList.add('card-picture-container');
     }
+      // Decorate content
+      const con = el.querySelector(':scope > div:not([class]) > div');
+      if (!con) return;
+      con.classList.add('card-content-container');
+      con.after(picPara);
   }
-  // Decorate content
-  const con = el.querySelector(':scope > div:not([class]) > div');
-  if (!con) return;
-  con.classList.add('card-content-container');
-  con.after(picPara);
-
   // Decorate CTA
   const ctaPara = inner.querySelector(':scope > div:last-of-type > p:last-of-type');
   if (!ctaPara) return;
