98c12a2439c42c0ee0cfba325584533d6d9ea1d5
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-03-16 15:42:08 -0700

    did this do it

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -8,12 +8,15 @@ export default function init(el) {
     if (picPara) {
       picPara.classList.add('card-picture-container');
     }
-      // Decorate content
-      const con = el.querySelector(':scope > div:not([class]) > div');
-      if (!con) return;
-      con.classList.add('card-content-container');
-      //con.after(picPara);
   }
+
+   // Decorate content
+   const piccont = el.querySelctor('card-icture-container');
+   const con = el.querySelector(':scope > div:not([class]) > div');
+   if (!con) return;
+   con.classList.add('card-content-container');
+   con.after(piccont);
+
   // Decorate CTA
   const ctaPara = inner.querySelector(':scope > div:last-of-type > p:last-of-type');
   if (!ctaPara) return;
